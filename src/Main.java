51dbed605ac6a265b56fda1dcbda45cdb3c2b2e7
Tom <gts-2010@mail.ru>
2023-01-27 13:38:54 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 5000;
+
+        int amount = 20;
+
+        int mile = price / amount ;
+
+        System.out.println("Количество милей:" + mile);
+
+    }
+}
