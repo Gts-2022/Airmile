f3be78aa5eeccb11093959ee00767cd4a8fc2d22
Tom <gts-2010@mail.ru>
2023-01-30 17:06:42 +0500

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+public int calculate(int cost) {
+    int amount;
+    amount = 20;
+    return cost / amount ;
+}
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        int price = 5000;
-
-        int amount = 20;
-
-        int mile = price / amount ;
-
-        System.out.println("Количество милей:" + mile);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
+
